b1eeaf3b32b357ae3aa360020085e3a5000707d7
aqsinehmedli <aqsinhmdli03@gmail.com>
2024-10-12 21:58:52 +0400

    Add project files.

---
+++ Client__UDP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+Console.WriteLine("Client:");
+
+var client = new UdpClient();
+var endPoint = new IPEndPoint(IPAddress.Loopback, 27001);
+
+try
+{
+    var filePath = @"C:\Users\User\OneDrive - ITM STEP MMC\Desktop\Walpaper\.IMG_20230801_222634.jpg";
+
+    if (!File.Exists(filePath))
+    {
+        Console.WriteLine("File does not exist.");
+        return;
+    }
+
+    using (var readF = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+    {
+        var buffer = new byte[1024];
+        int len;
+        while ((len = readF.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            client.Send(buffer, len, endPoint);
+            Console.WriteLine($"Sent {len} bytes...");
+        }
+    }
+
+    Console.WriteLine("File transfer complete.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+finally
+{
+    client.Close();
+}
---
+++ Server__UDP/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Server:");
+var server = new UdpClient(27001);
+var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+try
+{
+    while (true)
+    {
+        Console.WriteLine("Waiting for a message...");
+
+        var bytes = server.Receive(ref remoteEndPoint);
+        Console.WriteLine($"Received {bytes.Length} bytes from {remoteEndPoint.Address}:{remoteEndPoint.Port}");
+
+        var path = @"C:\Users\User\OneDrive - ITM STEP MMC\Desktop\Walpaper\received_image.jpg";
+
+        using (var writeF = new FileStream(path, FileMode.Append, FileAccess.Write))
+        {
+            writeF.Write(bytes, 0, bytes.Length);
+            Console.WriteLine("Image downloaded...");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+finally
+{
+    server.Close();
+}
